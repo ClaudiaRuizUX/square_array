9f448309f110f54cf749da4b390fc0ec3e1aab46
Claudia <claudiaruizux@gmail.com>
2022-06-14 20:06:49 +1000

    completes lab

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  results = []
+  array.each{ |element| results.push(element**2) }
+  results 
 end
+
+puts square_array([2,3,4])
+
+#each method
+#returns a new array
